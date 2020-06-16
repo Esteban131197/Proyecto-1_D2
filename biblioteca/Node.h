84bf0bcbce03743f4cae9dab1fe97b4d835d35ec
BrayanSolanoF <brayan.sf8@gmail.com>
2020-06-16 17:04:02 -0600

    Documentacion Nodo.h

--- biblioteca/Node.h
+++ biblioteca/Node.h
@@ -1,5 +1,11 @@
 
-
+/**
+ * @file Node.h
+ * @date 28/05/20
+ * @author Brayan Solano
+ * @brief Define un tipo ed variable para almacenar datos
+ * 
+*/
 
 using namespace std;
 template <typename T>
@@ -12,16 +18,61 @@ private:
     int references = 1;
 
 public:
+    /**
+     * @brief getData Obtiene dato almacenado en atributo T*
+     * @return Dato generico almacenado en puntero 
+    */
     T getData();
+    /**
+     * @brief getMemDir Obtiene el puntero T*
+     * @return Puntero
+    */
     T* getMemDir();
+    /**
+     * @brief getNext Obtiene el nodo de tipo puntero el cual apunta a un dato de otro nodo
+     * @param Puntero nodo
+    */
     Node* getNext();
+    /**
+     * @brief setData Asigna un nuevo dato al puntero T*
+     * @param newData Data que es asignado
+    */
     void setData(T* newData);
+    /**
+     * @brief setNext Asigna el nuevo puntero al cual se va a referenciar
+     * @param next puntero al cual se va a apuntar
+    */
     void setNext(Node* next);
+    /**
+     * @brief setID  Asigna un nuevo int id al nodo
+     * @param newID int el cual sera el id del nuevo nodo
+    */
     void setID(int newID);
+    /**
+     * @brief getID
+     * @return int que representa un identificador de cada nodo
+    */
     int getID();
+    /**
+     * @brief setDataType asigna un string que indica el tipo de dato almacenado en puntero T*
+     * @param newDataType  string que indica si es int, string o bool
+     * 
+    */
     void setDataType(string newDataType);
+    /**
+     * @brief getDataType Obtiene string que indica el tipo de dato almacenado
+     * @return string del tipo de dato
+    */
     string getDataType();
+    /**
+     * @brief getReferences Obtiene int de la cantidad de referencias que posee cada nodo
+     * @return int cantidad de referencias
+    */
     int getReferences();
+    /**
+     * @brief setReferences Asigna la cantidad de referencias que posee cada nodo
+     * @param newRef int que indica cantidad de referencias
+    */
     void setReferences(int newRef);
 
 };
